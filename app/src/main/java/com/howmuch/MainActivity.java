1d14b8d4a3123c60b81cbb685095f8c9623ff065
Ryan Russell <ryan.russell2000@gmail.com>
2019-03-02 21:24:41 -0800

    S

--- app/src/main/java/com/howmuch/MainActivity.java
+++ app/src/main/java/com/howmuch/MainActivity.java
@@ -2,34 +2,68 @@ package com.howmuch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 4342;
     private Manager manager;
-    private FirebaseAuth mAuth;
+    private List<AuthUI.IdpConfig> providers;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mAuth = FirebaseAuth.getInstance();
+        providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
 
         manager = Manager.getManager();
-        User u = manager.getUser();
-        manager.saveUser(u);
     }
 
     @Override
     public void onStart() {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null) {
+
+        } else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
 //        updateUI(currentUser);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                User loggedIn = manager.getUser();
+
+            } else {
+
+            }
+        }
     }
 
 }
