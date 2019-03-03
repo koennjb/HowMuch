1471eec2ae7b9d3acdb2e4168e11c70b1ebc85ad
CuongVo <david.vo1067@gmail.com>
2019-03-02 20:40:39 -0800

    Saving in mean time

--- app/src/main/java/com/howmuch/MainActivity.java
+++ app/src/main/java/com/howmuch/MainActivity.java
@@ -2,13 +2,55 @@ package com.howmuch;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int TEXT_RECO_REQ_CODE = 100;
+
+    private static final SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90);
+        ORIENTATIONS.append(Surface.ROTATION_90, 0);
+        ORIENTATIONS.append(Surface.ROTATION_180, 270);
+        ORIENTATIONS.append(Surface.ROTATION_270, 180);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    public void textRecog(View view) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,TEXT_RECO_REQ_CODE);
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if(requestCode==TEXT_RECO_REQ_CODE)
+        {
+            if(resultCode==RESULT_OK)
+            {
+                Bitmap photo= (Bitmap)data.getExtras().get("data");
+            }
+            else if(resultCode == RESULT_CANCELED)
+            {
+                Toast.makeText(this, "Failed to Capture", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
 }
+
